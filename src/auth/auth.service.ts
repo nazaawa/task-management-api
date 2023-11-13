f8b731bb4781843124a2f3a858d92792d9c48636
Johnny Andedi <johnnyetsha@gmail.com>
2023-11-13 04:17:26 +0100

    * feat(auth): add UserRepository to AuthModule imports * feat(auth): add signUp method to AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './users.repository';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
 export class AuthService {
@@ -8,4 +9,9 @@ export class AuthService {
         @InjectRepository(UserRepository)
         private userRepository : UserRepository,
     ){}
+
+
+    async signUp(authCredentialsDto :  AuthCredentialsDto) :  Promise<void>{
+        this.userRepository.createUser(authCredentialsDto);
+    }
 }
